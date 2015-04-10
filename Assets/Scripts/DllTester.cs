5e27a07dd3ee3b544de5c00292241be22413eb1b
Maximilian Wandinger <ga49vas@mytum.de>
2015-04-10 12:22:29 +0200

    Added Seilzugmaschine, zmq dlls

---
+++ Assets/Scripts/DllTester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Unity_ZMQ_Client;
+
+public class DllTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //Class1.InitNW();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
