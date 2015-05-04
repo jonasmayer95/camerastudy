fbf266bde51cef87de4e1ef998808f374b1b0f28
Krzysztof Lesiak <krzysztof.lesiak@tum.de>
2015-05-04 20:49:36 +0200

    fixed UI component setup prior to tomorrow's testing

--- Assets/Scripts/ConnectUI.cs
+++ Assets/Scripts/ConnectUI.cs
@@ -11,15 +11,18 @@ public class ConnectUI : MonoBehaviour
     void Start()
     {
         comm = GetComponent<ServerCommunication>();
-        addressInput = transform.FindChild("AddressInputField").GetComponent<Text>();
-        topicInput = transform.FindChild("TopicInputField").GetComponent<Text>();
+        addressInput = transform.FindChild("AddressInputField").GetChild(1).GetComponent<Text>();
+        topicInput = transform.FindChild("TopicInputField").GetChild(1). GetComponent<Text>();
     }
 
     public void Connect()
     {
         //check both strings for correct formatting and pass them on
         //as args for ServerCommunication
+        string topic = topicInput.text;
+        string url = addressInput.text;
 
         //comm.SetupConnection(topic, url);
+        Debug.Log(string.Format("Connecting to {0}, subscribing to {1}", url, topic));
     }
 }
