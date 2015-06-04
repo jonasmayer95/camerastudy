62a74052be33de3eff193b93649aab11cf82545a
Krzysztof Lesiak <krzysztof.lesiak@tum.de>
2015-06-04 11:00:14 +0200

    - added some PODs that map directly to json - added instructions for further progress; look them up in   ServerCommmunication.cs

---
+++ Assets/Scripts/InseilTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Plain old data types that directly map to the JSON data
+/// we get from the server.
+/// </summary>
+
+struct InseilPosition
+{
+    public double x;
+    public double y;
+    public double z;
+}
+
+struct InseilRotation
+{
+    public double x;
+    public double y;
+    public double z;
+    public double w;
+}
+
+struct InseilJoint
+{
+    public InseilPosition position;
+    public InseilRotation rotation;
+}
+
+class InseilMeasurement
+{
+    //I used a dictionary because typing in all the joints is
+    //not very flexible w.r.t. protocol changes, not to mention
+    //it is way too much work for nothing in return.
+    public Dictionary<string, InseilJoint> data;
+    public UInt64 timestamp;
+
+}
+class InseilMessage
+{
+    public UInt32 version;
+    public string id;
+    public InseilMeasurement measurement;
+    public UInt64 sendtime;
+}
+
--- Assets/Scripts/ServerCommunication.cs
+++ Assets/Scripts/ServerCommunication.cs
@@ -2,30 +2,48 @@
 using System.Collections;
 using NetMQ;
 using NetMQ.Sockets;
+using FullSerializer;
 
 public class ServerCommunication : MonoBehaviour
 {
     private static NetMQContext ctx;
     private static SubscriberSocket client;
-
-    private NetMQMessage message;
+    private NetMQMessage serverMessage;
     private bool recv;
 
+    private static readonly fsSerializer serializer = new fsSerializer();
+    private InseilMessage message;
 
     void Update()
     {
-        recv = client.TryReceiveMultipartMessage(ref message);
+        recv = client.TryReceiveMultipartMessage(ref serverMessage);
 
         if (recv)
         {
-            Debug.Log(string.Format("Frames: {0}", message.FrameCount));
-            foreach (NetMQFrame fr in message)
+            Debug.Log(string.Format("Frames: {0}", serverMessage.FrameCount));
+            foreach (NetMQFrame fr in serverMessage)
             {
                 Debug.Log(fr.ConvertToString());
             }
             
             //TODO: get message as string, feed it into a json parser and pass
             //the relevant data to the visualization component
+            //you should find some useful info on how to do that here:
+            //https://github.com/jacobdufault/fullserializer
+            
+            //the access pattern for single joints would be e.g.:
+            //message.measurement.data["spinebase"].position.x
+            //deserialize into the message class variable and you're good to go.
+
+            //i'm not quite sure how the serializer will handle the dictionary,
+            //but i think it should be rather straightforward. don't take my word
+            //for it, though.
+
+            //testing might be a bit of a problem since we don't have a server up and running,
+            //but you could just copy/paste that json from the protocol spec into my
+            //server sample (look it up at Doc/) and send that every second, just to verify
+            //the serializer is not doing some weird shit.
+
         }
     }
 
