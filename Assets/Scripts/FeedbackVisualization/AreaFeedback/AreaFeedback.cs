a69d569e1c0ac97b71fda9fbf5b198afcc47003f
Florian <ga97mug@mytum.de>
2015-08-31 15:08:40 +0200

    Area feedback updated arrow

--- Assets/Scripts/FeedbackVisualization/AreaFeedback/AreaFeedback.cs
+++ Assets/Scripts/FeedbackVisualization/AreaFeedback/AreaFeedback.cs
@@ -22,6 +22,8 @@ public class AreaFeedback : InseilFeedback {
     public ColoringType colorType;
     public int numTriangles;
 
+    private Material arrowMat;
+
     // Angular
     public float angle;
     public float radius;
@@ -33,10 +35,16 @@ public class AreaFeedback : InseilFeedback {
 
     private bool forward = true;
     // Geometry
-    private Mesh mesh; 
+    private Mesh mesh;
+    private Mesh mesh2;
     private Vector3[] vertices;
+    private Vector3[] vertices1;
     private Vector2[] uvs;
+    private Vector2[] uvs1;
     private int[] triangles;
+    private Vector3[] vertices2;
+    private Vector2[] uvs2;
+    private int[] triangles2;
     private Color[] colors;
     public Color startColor;
     public Color midColor;
@@ -44,11 +52,13 @@ public class AreaFeedback : InseilFeedback {
     public float midPoint;
     public float time;
     public Transform bone;
+    public float zSize;
 	// Use this for initialization
 	void Start () {
         orientation = Quaternion.Euler(rotation);
         InitializeFeedbackArea();
         time *= colors.Length/2 - 1;
+        arrowMat = GetComponent<MeshRenderer>().materials[1];
 	}
 	
 	// Update is called once per frame
@@ -57,17 +67,21 @@ public class AreaFeedback : InseilFeedback {
         if (forward)
         {
             midPoint += Time.deltaTime * time;
-            if (midPoint > colors.Length / 2)
+            arrowMat.SetTextureOffset("_MainTex", new Vector2(arrowMat.GetTextureOffset("_MainTex").x, arrowMat.GetTextureOffset("_MainTex").y - Time.deltaTime));
+            if (midPoint > vertices1.Length / 2)
             {
                 forward = false;
+                arrowMat.SetTextureScale("_MainTex", new Vector2(arrowMat.GetTextureScale("_MainTex").x, -arrowMat.GetTextureScale("_MainTex").y));
             }
         }
         else 
         {
             midPoint -= Time.deltaTime * time;
+            arrowMat.SetTextureOffset("_MainTex", new Vector2(arrowMat.GetTextureOffset("_MainTex").x, arrowMat.GetTextureOffset("_MainTex").y - Time.deltaTime));
             if (midPoint < 0)
             {
                 forward = true;
+                arrowMat.SetTextureScale("_MainTex", new Vector2(arrowMat.GetTextureScale("_MainTex").x,-arrowMat.GetTextureScale("_MainTex").y));
             }
         }
         UpdateColor();
@@ -79,12 +93,20 @@ public class AreaFeedback : InseilFeedback {
     {
         if (type == AreaType.angular && numTriangles > 0)
         {
-            // Init arrays
+            // Init arrays submesh one
             numTriangles = (numTriangles+1)*2;
-            vertices = new Vector3[numTriangles + 4];
-            uvs = new Vector2[vertices.Length];
+            vertices1 = new Vector3[numTriangles + 4];
+            uvs1 = new Vector2[vertices1.Length];
             triangles = new int[numTriangles * 3];
-            colors = new Color[vertices.Length];
+
+            // Init arrays submesh two
+            vertices2 = new Vector3[numTriangles];
+            uvs2 = new Vector2[vertices2.Length];
+            triangles2 = new int[(numTriangles - 2) * 3];
+
+            colors = new Color[vertices1.Length + vertices2.Length];
+
+            
 
             // Calculate rotation angle and uvScaleFactor
             float angleStep = angle / (numTriangles/2 -1);
@@ -92,47 +114,90 @@ public class AreaFeedback : InseilFeedback {
 
 
             // Init pivot
-            vertices[0] = Vector3.zero;
-            vertices[vertices.Length / 2] = Vector3.zero;
-            uvs[0] = new Vector2(0.5f,0.5f);
-            uvs[vertices.Length / 2] = uvs[0];
-            Debug.Log(uvs[0]);
+            vertices1[0] = Vector3.zero;
+            vertices1[vertices1.Length / 2] = Vector3.zero;
+            uvs1[0] = new Vector2(0.5f,0.5f);
+            uvs1[vertices1.Length / 2] = uvs1[0];
             colors[0] = (startColor + endColor)/2;
-            colors[vertices.Length/2] = (startColor + endColor) / 2;
+            colors[vertices1.Length/2] = (startColor + endColor) / 2;
 
             for (int i = 0; i < numTriangles/2; i++)
             {
                 // Calculate vertices
-                vertices[i + 1] = Quaternion.AngleAxis(i * angleStep, Vector3.back) * Vector3.left;               
-                vertices[i + 1 + vertices.Length/2] = Quaternion.AngleAxis(i * angleStep, Vector3.forward) * Quaternion.AngleAxis(angle, Vector3.back) * Vector3.left;
-                vertices[i + 1].x = -vertices[i + 1].x;
-                vertices[i + 1 + vertices.Length / 2].x = -vertices[i + 1 + vertices.Length / 2].x;
+                vertices1[i + 1] = Quaternion.AngleAxis(i * angleStep, Vector3.back) * Vector3.left;               
+                vertices1[i + 1 + vertices1.Length/2] = Quaternion.AngleAxis(i * angleStep, Vector3.forward) * Quaternion.AngleAxis(angle, Vector3.back) * Vector3.left;
+                vertices1[i + 1].x = -vertices1[i + 1].x;
+                vertices1[i + 1 + vertices1.Length / 2].x = -vertices1[i + 1 + vertices1.Length / 2].x;
 
                 // Coloring vertices
                 if (i <= midPoint)
                 {
-                    colors[i + 1] = Color.Lerp(startColor, midColor, vertices[i + 1].y);
-                    colors[i + colors.Length / 2 + 1] = Color.Lerp(startColor, midColor, vertices[i + 1].y);
+                    colors[i + 1] = Color.Lerp(startColor, midColor, vertices1[i + 1].y);
+                    colors[i + vertices1.Length / 2 + 1] = Color.Lerp(startColor, midColor, vertices1[i + 1].y);
                 }
                 else
                 {
-                    colors[i + 1] = Color.Lerp(midColor, endColor, vertices[i + 1].y);
-                    colors[i + colors.Length / 2 + 1] = Color.Lerp(midColor, endColor, vertices[i + 1].y);
+                    colors[i + 1] = Color.Lerp(midColor, endColor, vertices1[i + 1].y);
+                    colors[i + vertices1.Length / 2 + 1] = Color.Lerp(midColor, endColor, vertices1[i + 1].y);
                 }
 
                 // Calculate uv coordinates
-                uvs[i + 1] = uvs[0] + (Vector2) (uvScaleFactor * vertices[i+1]);
-                uvs[i + 1 + vertices.Length/2] = uvs[0] + (Vector2)(uvScaleFactor * vertices[uvs.Length/2 + i + 1]);
+                uvs1[i + 1] = uvs1[0] + (Vector2) (uvScaleFactor * vertices1[i+1]);
+                uvs1[i + 1 + vertices1.Length/2] = uvs1[0] + (Vector2)(uvScaleFactor * vertices1[uvs1.Length/2 + i + 1]);
 
                 // Calculate triangles
                 triangles[i * 3] = i + 1;
                 triangles[i * 3 + 1] = i + 2;
                 triangles[i * 3 + 2] = 0;
-                triangles[i * 3 + triangles.Length / 2] = i + 1 + vertices.Length/2;
-                triangles[i * 3 + 1 + triangles.Length / 2] = i + 2 + vertices.Length/2;
-                triangles[i * 3 + 2 + triangles.Length / 2] = vertices.Length/2;
+                triangles[i * 3 + triangles.Length / 2] = i + 1 + vertices1.Length/2;
+                triangles[i * 3 + 1 + triangles.Length / 2] = i + 2 + vertices1.Length/2;
+                triangles[i * 3 + 2 + triangles.Length / 2] = vertices1.Length/2;
 
             }
+            int k = 1;
+            for(int i = 0; i < vertices2.Length; i = i+2)
+            {
+                vertices2[i] = new Vector3(vertices1[k].x, vertices1[k].y, vertices1[k].z + zSize);
+                vertices2[i + 1] = new Vector3(vertices1[k].x, vertices1[k].y, vertices1[k].z - zSize);
+
+                uvs2[i] = new Vector2(0, i / (0.5f * vertices2.Length));
+                uvs2[i + 1] = new Vector2(1, i / (0.5f * vertices2.Length));          
+
+                k++;
+            }
+            for (int i = 0; i < triangles2.Length/3; i = i+2)
+            {
+                //first triangle
+                triangles2[i * 3] = i + vertices1.Length;
+                triangles2[i * 3 + 1] = i + 1 + vertices1.Length;
+                triangles2[i * 3 + 2] = i + 3 + vertices1.Length;
+                //second triangle
+                triangles2[(i + 1) * 3] = i + vertices1.Length;
+                triangles2[(i + 1) * 3 + 1] = i + 3 + vertices1.Length;
+                triangles2[(i + 1) * 3 + 2] = i + 2 + vertices1.Length;
+            }
+                
+            vertices = new Vector3[vertices1.Length + vertices2.Length];
+            {
+                for(int i = 0; i < vertices1.Length; i++)
+                {
+                    vertices[i] = vertices1[i];
+                }
+                for(int i = vertices1.Length; i < vertices.Length; i++)
+                {
+                    vertices[i] = vertices2[i - vertices1.Length];
+                }
+            }
+            uvs = new Vector2[uvs1.Length + uvs2.Length];
+            for(int i = 0; i < uvs1.Length; i++)
+            {
+                uvs[i] = uvs1[i];
+            }
+            for(int i = uvs1.Length; i < uvs.Length; i++)
+            {
+                uvs[i] = uvs2[i - uvs1.Length];
+            }
+            
         }
 
         if (type == AreaType.linear)
@@ -182,9 +247,14 @@ public class AreaFeedback : InseilFeedback {
     {
         // Build mesh
         mesh = new Mesh();
+        mesh.subMeshCount = 2;
         CreateGeometry(areaType);
-        GetComponent<MeshFilter>().mesh = mesh;
+       
         ApplyGeometry();
+
+        mesh.SetTriangles(triangles, 0);
+        mesh.SetTriangles(triangles2, 1);
+        GetComponent<MeshFilter>().mesh = mesh;
         
         CalculateTransform();
 
@@ -194,7 +264,7 @@ public class AreaFeedback : InseilFeedback {
     {
         mesh.vertices = vertices;
         mesh.uv = uvs;
-        mesh.triangles = triangles;
+        //mesh.triangles = triangles;
         mesh.colors = colors;
     }
 
@@ -214,68 +284,68 @@ public class AreaFeedback : InseilFeedback {
     void UpdateColor()
     {
         colors[0] = Color.black;
-        colors[colors.Length / 2] = Color.black;
+        colors[vertices1.Length / 2] = Color.black;
 
         if (colorType == ColoringType.continious)
         // Continous coloring
         {
-            for (int i = 1; i < colors.Length / 2; i++)
+            for (int i = 1; i < vertices1.Length / 2; i++)
             {
                 if (i <= midPoint)
                 {
                     colors[i] = Color.Lerp(startColor, midColor, (float)i / midPoint);
-                    colors[colors.Length -i] = Color.Lerp(startColor, midColor, (float)i / midPoint);
+                    colors[vertices1.Length -i] = Color.Lerp(startColor, midColor, (float)i / midPoint);
                 }
                 else
                 {
-                    colors[i] = Color.Lerp(midColor, endColor, ((float)i - midPoint) / (colors.Length / 2 - 1 - midPoint));
-                    colors[colors.Length - i] = Color.Lerp(midColor, endColor, ((float)i - midPoint) / (colors.Length / 2 - 1 - midPoint));
+                    colors[i] = Color.Lerp(midColor, endColor, ((float)i - midPoint) / (vertices1.Length / 2 - 1 - midPoint));
+                    colors[vertices1.Length - i] = Color.Lerp(midColor, endColor, ((float)i - midPoint) / (vertices1.Length / 2 - 1 - midPoint));
                 }
             }
         }
         else if (colorType == ColoringType.jumping)
         {
             // jumping coloring
-            for (int i = 1; i < colors.Length / 2; i++)
+            for (int i = 1; i < vertices1.Length / 2; i++)
             {
                 colors[i] = startColor;
-                colors[colors.Length/2 - 1 -i] = startColor;
+                colors[vertices1.Length/2 - 1 -i] = startColor;
             }
             colors[(int)midPoint] = midColor;
-            colors[((int)midPoint + 1) % (colors.Length / 2)] = midColor;
-            colors[((int)midPoint - 1 + colors.Length / 2) % (colors.Length / 2)] = midColor;
-            colors[colors.Length - 1 - (int)midPoint] = midColor;
-            colors[colors.Length / 2 - 1 - ((int)midPoint + 1) % (colors.Length / 2) + colors.Length / 2] = midColor;
-            colors[colors.Length / 2 - 1 - ((int)midPoint - 1 + colors.Length / 2) % (colors.Length / 2) + colors.Length / 2] = midColor;
+            colors[((int)midPoint + 1) % (vertices1.Length / 2)] = midColor;
+            colors[((int)midPoint - 1 + vertices1.Length / 2) % (vertices1.Length / 2)] = midColor;
+            colors[vertices1.Length - 1 - (int)midPoint] = midColor;
+            colors[vertices1.Length / 2 - 1 - ((int)midPoint + 1) % (vertices1.Length / 2) + vertices1.Length / 2] = midColor;
+            colors[vertices1.Length / 2 - 1 - ((int)midPoint - 1 + vertices1.Length / 2) % (vertices1.Length / 2) + vertices1.Length / 2] = midColor;
             colors[0] = Color.black;
-            colors[colors.Length / 2] = Color.black;
+            colors[vertices1.Length / 2] = Color.black;
         }
         else if (colorType == ColoringType.interpolate)
         {
-            int range = (int)((colors.Length / 2 - 1) * midColorRange);
-            for(int i = 1; i < colors.Length/2; i++)
+            int range = (int)((vertices1.Length / 2 - 1) * midColorRange);
+            for(int i = 1; i < vertices1.Length/2; i++)
             {
                 if((int)midPoint - range > i+1)
                 {
                     colors[i] = startColor;
-                    colors[colors.Length  - i] = startColor;
+                    colors[vertices1.Length  - i] = startColor;
                 }
                 else if ((int)midPoint + range < i)
                 {
                     colors[i] = endColor;
-                    colors[colors.Length  - i] = endColor;
+                    colors[vertices1.Length  - i] = endColor;
                 }
                 else
                 {
                     if (i < midPoint)
                     {
                         colors[i] = Color.Lerp(startColor, midColor, (i - (midPoint - range)) / (2 * range));
-                        colors[colors.Length  - i] = Color.Lerp(startColor, midColor, (i - (midPoint - range)) / (2 * range));
+                        colors[vertices1.Length  - i] = Color.Lerp(startColor, midColor, (i - (midPoint - range)) / (2 * range));
                     }
                     else
                     {
                         colors[i] = Color.Lerp(midColor, endColor, 1 - ((midPoint + range) - i) / (2 * range));
-                        colors[colors.Length  - i] = Color.Lerp(midColor, endColor, 1 - ((midPoint + range) - i) / (2 * range));
+                        colors[vertices1.Length  - i] = Color.Lerp(midColor, endColor, 1 - ((midPoint + range) - i) / (2 * range));
                         
                     }
                 }
