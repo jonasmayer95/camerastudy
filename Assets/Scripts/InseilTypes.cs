6548e454436748a8ff3c301f69ee5cb0d0464f46
NUC <nuc@idonotexist.net>
2015-09-08 18:17:30 +0200

    Adding work in progress. To do: replace JSON parser/serializer as the current one is fucking slow.

--- Assets/Scripts/InseilTypes.cs
+++ Assets/Scripts/InseilTypes.cs
@@ -40,6 +40,20 @@ public struct InseilRotation
 [fsObject(Converter = typeof(InseilJointConverter))]
 public struct InseilJoint
 {
+    public InseilJoint(double px, double py, double pz, double rx, double ry, double rz, double rw)
+    {
+        position = new InseilPosition();
+        position.x = px;
+        position.y = py;
+        position.z = pz;
+
+        rotation = new InseilRotation();
+        rotation.x = rx;
+        rotation.y = ry;
+        rotation.z = rz;
+        rotation.w = rw;
+    }
+
     public InseilPosition position;
     public InseilRotation rotation;
 
@@ -49,34 +63,45 @@ public struct InseilJoint
     }
 }
 
-class InseilJointConverter : fsDirectConverter<InseilJoint>
+class InseilJointConverter : fsDirectConverter/*<InseilJoint>*/
 {
+    Dictionary<string, fsData> tmpDict = new Dictionary<string, fsData>(4);
+
+    public override Type ModelType
+    {
+        get { return typeof(InseilJoint); }
+    }
+
     public override object CreateInstance(fsData data, Type storageType)
     {
         return new InseilJoint();
     }
 
-    protected override fsResult DoSerialize(InseilJoint model, Dictionary<string, fsData> serialized)
+    //protected override fsResult DoSerialize(InseilJoint model, Dictionary<string, fsData> serialized)
+    //{
+    //    Dictionary<string, fsData> tmpDict = new Dictionary<string, fsData>(3);
+    //    tmpDict["x"] = new fsData(model.position.x);
+    //    tmpDict["y"] = new fsData(model.position.y);
+    //    tmpDict["z"] = new fsData(model.position.z);
+    //    serialized["p"] = new fsData(tmpDict);
+    //    tmpDict.Clear();
+
+    //    tmpDict["x"] = new fsData(model.rotation.x);
+    //    tmpDict["y"] = new fsData(model.rotation.y);
+    //    tmpDict["z"] = new fsData(model.rotation.z);
+    //    tmpDict["w"] = new fsData(model.rotation.w);
+    //    serialized["r"] = new fsData(tmpDict);
+
+    //    return fsResult.Success;
+    //}
+
+    public override fsResult TrySerialize(object instance, out fsData serialized, Type storageType)
     {
-        Dictionary<string, fsData> posDict = new Dictionary<string, fsData>(3);
-        posDict["x"] = new fsData(model.position.x);
-        posDict["y"] = new fsData(model.position.y);
-        posDict["z"] = new fsData(model.position.z);
-        serialized["p"] = new fsData(posDict);
-
-        Dictionary<string, fsData> rotDict = new Dictionary<string, fsData>(4);
-        rotDict["x"] = new fsData(model.rotation.x);
-        rotDict["y"] = new fsData(model.rotation.y);
-        rotDict["z"] = new fsData(model.rotation.z);
-        rotDict["w"] = new fsData(model.rotation.w);
-        serialized["r"] = new fsData(rotDict);
-
-        return fsResult.Success;
+        throw new NotImplementedException();
     }
 
-    protected override fsResult DoDeserialize(Dictionary<string, fsData> data, ref InseilJoint model)
+    public override fsResult TryDeserialize(fsData data, ref object instance, Type storageType)
     {
-        //get p and r, stuff them into a new InseilJoint
         var result = fsResult.Success;
 
         fsData posData;
@@ -88,24 +113,63 @@ class InseilJointConverter : fsDirectConverter<InseilJoint>
         if ((result += CheckType(rotData, fsDataType.Object)).Failed) return result;
 
         var dict = posData.AsDictionary;
-        model.position.x = dict["x"].AsDouble;
-        model.position.y = dict["y"].AsDouble;
-        model.position.z = dict["z"].AsDouble;
+        double px = dict["x"].AsDouble;
+        double py = dict["y"].AsDouble;
+        double pz = dict["z"].AsDouble;
 
         dict = rotData.AsDictionary;
-        model.rotation.x = dict["x"].AsDouble;
-        model.rotation.y = dict["y"].AsDouble;
-        model.rotation.z = dict["z"].AsDouble;
-        model.rotation.w = dict["w"].AsDouble;
+        double rx = dict["x"].AsDouble;
+        double ry = dict["y"].AsDouble;
+        double rz = dict["z"].AsDouble;
+        double rw = dict["w"].AsDouble;
+
+        instance = new InseilJoint(px, py, pz, rx, ry, rz, rw);
 
         return result;
     }
+
+    //protected override fsResult DoDeserialize(Dictionary<string, fsData> data, ref InseilJoint model)
+    //{
+    //    //get p and r, stuff them into a new InseilJoint
+    //    var result = fsResult.Success;
+
+    //    fsData posData;
+    //    if ((result += CheckKey(data, "p", out posData)).Failed) return result;
+    //    if ((result += CheckType(posData, fsDataType.Object)).Failed) return result;
+
+    //    fsData rotData;
+    //    if ((result += CheckKey(data, "r", out rotData)).Failed) return result;
+    //    if ((result += CheckType(rotData, fsDataType.Object)).Failed) return result;
+
+    //    var dict = posData.AsDictionary;
+    //    model.position.x = dict["x"].AsDouble;
+    //    model.position.y = dict["y"].AsDouble;
+    //    model.position.z = dict["z"].AsDouble;
+
+    //    dict = rotData.AsDictionary;
+    //    model.rotation.x = dict["x"].AsDouble;
+    //    model.rotation.y = dict["y"].AsDouble;
+    //    model.rotation.z = dict["z"].AsDouble;
+    //    model.rotation.w = dict["w"].AsDouble;
+
+    //    return result;
+    //}
 }
 
+//the problem seems to be the deserializer attempts to fill the dictionary with the same keys
+//TODO: verify this and if that's the case, clear the dict before deserializing
+//TODO: try this one for parsing/deserializing: http://wiki.unity3d.com/index.php/JSONObject
 public class InseilMeasurement
 {
     public Dictionary<string, InseilJoint> data;
     public UInt64 timestamp;
+    private const int JointCount = 28;
+
+    public InseilMeasurement()
+    {
+        this.data = new Dictionary<string, InseilJoint>(JointCount);
+        this.timestamp = 0;
+    }
 
     public override string ToString()
     {
@@ -120,6 +184,11 @@ public class InseilMessage
     public string id;
     public InseilMeasurement measurement;
     public UInt64 sendtime;
+
+    public InseilMessage()
+    {
+        measurement = new InseilMeasurement();
+    }
 
     public override string ToString()
     {
