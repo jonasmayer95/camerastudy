6d9aa236ba7dc96543f560ba53be1cf7c928601e
NUC <nuc@idonotexist.net>
2015-09-15 17:46:03 +0200

    added camerafeedback: cylinder 1.0

--- Assets/Scripts/FeedbackVisualization/CameraFeedback/CameraFeedback.cs
+++ Assets/Scripts/FeedbackVisualization/CameraFeedback/CameraFeedback.cs
@@ -8,18 +8,31 @@ public enum CameraFeedbackMode
 
 public class CameraFeedback : InseilFeedback {
 
+    // Camera Setup
     public float camDistance;
-    public Transform feedbackCamera;
+    public Camera feedbackCamera;
+
+    // Joint setup
     public Transform feedbackAvatar_hip;
     public Transform feedbackAvatar_joint;
     public Transform connectingJoint;
+
+    // Target position and tolerance
     public Vector3 relTargetPos;
     public float tolerance;
-    public GameObject arrow3DPrefab;
+
+    // Feedback mode
+    public CameraFeedbackMode cameraFeedbackMode;
+
+    // Prefabs
     public GameObject targetSpherePrefab;
     public Color targetSphereColor;
+    public GameObject arrow3DPrefab;
+    public GameObject cylinderPrefab;    
     public GameObject line3DPrefab;
-    public CameraFeedbackMode cameraFeedbackMode;
+    
+    // Variables used for referencing
+    private GameObject feedbackCylinder;    
     private GameObject targetSphere;
     private GameObject arrow3D;
     private GameObject line3D;
@@ -29,67 +42,100 @@ public class CameraFeedback : InseilFeedback {
 	// Use this for initialization
     void Start()
     {
-        if (cameraFeedbackMode == CameraFeedbackMode.LinearArrow)
-        {
-            targetSphere = Instantiate(targetSpherePrefab, feedbackAvatar_hip.position + relTargetPos, Quaternion.identity) as GameObject;
-            targetSphere.GetComponent<Renderer>().material.color = targetSphereColor;
-            targetSphere.transform.parent = transform;
-            targetSphere.SetActive(false);
+        // Spawn a sphere to show the target position for debugging
+        targetSphere = Instantiate(targetSpherePrefab, feedbackAvatar_hip.position + relTargetPos, Quaternion.identity) as GameObject;
+        targetSphere.GetComponent<Renderer>().material.color = targetSphereColor;
+        targetSphere.transform.parent = transform;
+        targetSphere.SetActive(false);
 
-            arrow3D = Instantiate(arrow3DPrefab, feedbackAvatar_joint.position + ((feedbackAvatar_hip.position + relTargetPos) - feedbackAvatar_joint.position) / 2.0f, Quaternion.identity) as GameObject;
-            arrow3D.transform.parent = transform;
-            arrow3D.SetActive(false);
+        // Spawn a linear arrow pointing from the joint to the correct position
+        arrow3D = Instantiate(arrow3DPrefab, feedbackAvatar_joint.position + ((feedbackAvatar_hip.position + relTargetPos) - feedbackAvatar_joint.position) / 2.0f, Quaternion.identity) as GameObject;
+        arrow3D.transform.parent = transform;
+        arrow3D.SetActive(false);
 
-            line3D = Instantiate(line3DPrefab, transform.position, Quaternion.identity) as GameObject;
-            line3D.transform.parent = transform;
-            rend = line3D.GetComponent<LineRenderer>();
-            rend.SetVertexCount(2);
-            rend.SetPosition(0, transform.position);
-            rend.SetPosition(1, connectingJoint.position);
+        // Spawn the line pointing from the joint to the camera image
+        line3D = Instantiate(line3DPrefab, transform.position, Quaternion.identity) as GameObject;
+        line3D.transform.parent = transform;
+        rend = line3D.GetComponent<LineRenderer>();
+        rend.SetVertexCount(2);
+        rend.SetPosition(0, transform.position);
+        rend.SetPosition(1, connectingJoint.position);
 
-            UpdateCameraPosition();
-        }
+        // Spawn a cylinder showing an bent arrow from the joint to the correct position
+        feedbackCylinder = Instantiate(cylinderPrefab, feedbackAvatar_joint.position + ((feedbackAvatar_hip.position + relTargetPos) - feedbackAvatar_joint.position) / 2.0f, Quaternion.identity) as GameObject;
+        feedbackCylinder.transform.parent = transform;
+        feedbackCylinder.SetActive(false);
 
-        if (cameraFeedbackMode == CameraFeedbackMode.Cylinder)
-        {
-
-        }
+        // Init camera position
+        UpdateCameraPosition();
     }
 
     void OnEnable()
     {
         //UpdateCameraPosition();
     }
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        if (cameraFeedbackMode == CameraFeedbackMode.LinearArrow)
-        {
-            if (Vector3.Distance(feedbackAvatar_joint.position - feedbackAvatar_hip.position, relTargetPos) > tolerance)
-            {
-                targetSphere.SetActive(true);
-                targetSphere.transform.position = feedbackAvatar_hip.position + relTargetPos;
 
+    // Update is called once per frame
+    void Update()
+    {
+        // Show the arrow only when out of tolerance
+        if (Vector3.Distance(feedbackAvatar_joint.position - feedbackAvatar_hip.position, relTargetPos) > tolerance)
+        {
+            // Show sphere at target position (for debugging)
+            targetSphere.SetActive(true);
+            targetSphere.transform.position = feedbackAvatar_hip.position + relTargetPos;
+
+            // Update the position and orientation of the 3D arrow
+            if (cameraFeedbackMode == CameraFeedbackMode.LinearArrow)
+            {
+                // Deactivate the cylinder
+                feedbackCylinder.SetActive(false);
+
+                // Update and activate the 3D arrow
                 arrow3D.transform.position = feedbackAvatar_joint.position + ((feedbackAvatar_hip.position + relTargetPos) - feedbackAvatar_joint.position) / 2.0f;
                 arrow3D.transform.LookAt(targetSphere.transform.position);
+
+                // Activate the 3D arrow
                 arrow3D.SetActive(true);
             }
 
-            else
+            // Update the position size and orientation of the cylinder showing a bent arrow
+            if (cameraFeedbackMode == CameraFeedbackMode.Cylinder)
             {
-                targetSphere.SetActive(false);
+                // Deactivate the 3D arrow
                 arrow3D.SetActive(false);
+
+                // Update position and size
+                feedbackCylinder.transform.position = feedbackAvatar_joint.position + ((feedbackAvatar_hip.position + relTargetPos) - feedbackAvatar_joint.position) / 2.0f;
+                float radius = Vector3.Distance(targetSphere.transform.position, feedbackAvatar_joint.position) / 2.0f;
+                feedbackCylinder.transform.localScale = new Vector3(radius, radius, radius);
+
+                // Update orientation
+                Vector2 projectedErrorVector;
+                projectedErrorVector = (targetSphere.transform.position - feedbackAvatar_joint.position).normalized;
+                Vector3 eulerOrientation = Vector3.Cross(projectedErrorVector, feedbackCamera.transform.forward.normalized);
+                Quaternion targetRotation = Quaternion.FromToRotation(feedbackCylinder.transform.up, eulerOrientation);
+                feedbackCylinder.transform.rotation = Quaternion.Slerp(feedbackCylinder.transform.rotation, targetRotation, Time.deltaTime * 10);
+
+                // Activate cylinder
+                feedbackCylinder.SetActive(true);
             }
-
-            UpdateCameraPosition();
-            rend.SetPosition(1, connectingJoint.position);
         }
-
-        if (cameraFeedbackMode == CameraFeedbackMode.Cylinder)
+        
+        // Within tolerance
+        else
         {
+            // Deactivate sphere and arrow
+            targetSphere.SetActive(false);
+            arrow3D.SetActive(false);
+            feedbackCylinder.SetActive(false);
+        }        
 
-        }
+        // Update Camera position
+        UpdateCameraPosition();
+
+        // Update the end point of the 3D line according to the new position of the joint
+        rend.SetPosition(1, connectingJoint.position);
 	}
 
     void UpdateCameraPosition()
@@ -107,12 +153,11 @@ public class CameraFeedback : InseilFeedback {
         boneVectorB = boneVectorB.normalized;        
 
         // Calculate normal for camera
-       // Vector3 camNormal = Vector3.Cross(arrowVector, ((boneVectorA + boneVectorB) - feedbackAvatar_joint.position).normalized);
-        Vector3 camNormal = Vector3.Cross(boneVectorA, boneVectorB);
+        // Vector3 camNormal = Vector3.Cross(arrowVector, ((boneVectorA + boneVectorB) - feedbackAvatar_joint.position).normalized);
+        //Vector3 camNormal = Vector3.Cross(boneVectorA, boneVectorB);
 
         //feedbackCamera.position = arrow3D.transform.position - camNormal.normalized * camDistance;
         //feedbackCamera.transform.LookAt(feedbackAvatar_joint.transform.position);
-        feedbackCamera.position = feedbackAvatar_joint.transform.position + camDistance * Vector3.forward;
+        feedbackCamera.transform.position = feedbackAvatar_joint.transform.position + camDistance * Vector3.forward;
     }
-
 }
