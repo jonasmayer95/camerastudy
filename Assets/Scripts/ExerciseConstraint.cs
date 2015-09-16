d2f5c5076e226d1bdc5811403481b92bd7fe0be0
Florian <ga97mug@mytum.de>
2015-09-16 16:04:48 +0200

    Exercise Files now reading and deserializing.

---
+++ Assets/Scripts/ExerciseConstraint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExerciseConstraint : MonoBehaviour {
+    
+    public Vector3 position;
+    public string joint;
+    public string relativeBone;
+    public string type;
+    public float tolerance;
+    
+    public ExerciseConstraint(double x, double y, double z, string joint, string relativeBone, string type, double tolerance)
+    {
+        this.position = new Vector3((float)x, (float)y, (float)z);
+        this.joint = joint;
+        this.relativeBone = relativeBone;
+        this.type = type;
+        this.tolerance = (float)tolerance;
+    }
+
+    public override string ToString()
+    {
+        return position.ToString() + " " + joint + " " + relativeBone + " " + type + " " + tolerance.ToString() + "\n";
+    }
+}
