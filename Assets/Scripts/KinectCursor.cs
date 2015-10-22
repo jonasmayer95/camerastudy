e9ab09e0ed53afcbc1770839749d9df37a426aae
Florian <ga97mug@mytum.de>
2015-10-22 15:38:04 +0200

    circular progress bar for kinect cursor

--- Assets/Scripts/KinectCursor.cs
+++ Assets/Scripts/KinectCursor.cs
@@ -10,16 +10,18 @@ public class KinectCursor : MonoBehaviour {
     public Transform relativeBone; //In this case should be the shoulder
     private Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
     private RectTransform rectTransform;
-    private Vector2 curserPos;
+    private Vector3 curserPos;
     public float selectionTime;
     private float selectionStartTime;
     private bool selecting;
     public EventSystem eventSystem;
+    private Image pic;
+    public RectTransform slider;
 
 	// Use this for initialization
 	void Start () {
         rectTransform = GetComponent<RectTransform>();
-	
+        pic = transform.GetChild(0).GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -32,7 +34,9 @@ public class KinectCursor : MonoBehaviour {
         curserPos.x = Mathf.Max(0, curserPos.x);
         curserPos.y = Mathf.Min(Screen.height, curserPos.y);
         curserPos.y = Mathf.Max(0, curserPos.y);
+        curserPos.z = 0;
         rectTransform.position = curserPos;
+        slider.position = rectTransform.position;
 
         
 	}
@@ -51,10 +55,15 @@ public class KinectCursor : MonoBehaviour {
     {
         if(other.tag == "UI")
         {
+            pic.fillAmount = (Time.time - selectionStartTime) / selectionTime;
             if(Time.time - selectionStartTime > selectionTime && selecting)
             {
+                
                 ExecuteEvents.Execute(other.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
+                selectionStartTime = Time.time;
+                pic.fillAmount = 0;
             }
+            
         }
     }
 
@@ -63,6 +72,7 @@ public class KinectCursor : MonoBehaviour {
         if(other.tag == "UI")
         {
             selecting = false;
+            pic.fillAmount = 0;
         }
     }
 }
