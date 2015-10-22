85fcd43c3dee6d9404b42981b0bbc177b7092c4a
NUC <nuc@idonotexist.net>
2015-10-22 14:06:34 +0200

    Added feedback camera rotation around y

--- Assets/Scripts/FeedbackVisualization/CameraFeedback/CameraFeedback.cs
+++ Assets/Scripts/FeedbackVisualization/CameraFeedback/CameraFeedback.cs
@@ -7,6 +7,16 @@ public enum CameraFeedbackMode
     LinearArrow, Cylinder, RigedArrow, DockingArrow2D
 }
 
+public enum CameraPerspectives
+{
+    Front, Up, Left, Right, Behind, Normal
+}
+
+public enum CameraMotionStates
+{
+    Jumping, Moving
+}
+
 public class CameraFeedback : MonoBehaviour {
 
     // Camera Setup
@@ -26,7 +36,9 @@ public class CameraFeedback : MonoBehaviour {
     private int index;
 
     // Feedback mode
-    public CameraFeedbackMode cameraFeedbackMode;
+    public CameraFeedbackMode cameraFeedbackMode = CameraFeedbackMode.RigedArrow;
+    public CameraPerspectives cameraPerspective = CameraPerspectives.Front;
+    public CameraMotionStates cameraMotion = CameraMotionStates.Jumping;
 
     // Prefabs
     public GameObject targetSpherePrefab;
@@ -92,12 +104,12 @@ public class CameraFeedback : MonoBehaviour {
 
         // Spawn a dockingStation to show the target position for debugging
         arrowDock2D = Instantiate(arrowDock2DPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-        arrowDock2D.transform.parent = transform;
+        arrowDock2D.transform.parent = feedbackCamera.transform;
         arrowDock2D.SetActive(false);
 
         // Spawn a dockingArrow pointing from the joint to the dock
         dockingArrow2D = Instantiate(dockingArrow2DPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-        dockingArrow2D.transform.parent = transform;
+        dockingArrow2D.transform.parent = feedbackCamera.transform;
         dockingArrow2D.SetActive(false);
 
         // Init camera position
@@ -280,13 +292,15 @@ public class CameraFeedback : MonoBehaviour {
                 dockingArrow2D.transform.position = feedbackCamera.transform.position + spriteDistance * (feedbackAvatar_joint.position - feedbackCamera.transform.position).normalized;
                 arrowDock2D.transform.position = feedbackCamera.transform.position + spriteDistance * (targetSphere.transform.position - feedbackCamera.transform.position).normalized;
 
-                Vector2 dir = arrowDock2D.transform.position - dockingArrow2D.transform.position;
-                dir.Normalize();
-                Quaternion arrowRot = Quaternion.FromToRotation(Vector3.right, dir);
-                dockingArrow2D.transform.rotation = Quaternion.Slerp(dockingArrow2D.transform.rotation, arrowRot, Time.deltaTime * 20);
+                dockingArrow2D.transform.localRotation = Quaternion.identity;
+                arrowDock2D.transform.localRotation = Quaternion.identity;                
 
-                Quaternion dockRot = Quaternion.FromToRotation(Vector3.left, -dir);
-                arrowDock2D.transform.rotation = Quaternion.Slerp(arrowDock2D.transform.rotation, dockRot, Time.deltaTime * 20);
+                Vector2 dir = arrowDock2D.transform.localPosition - dockingArrow2D.transform.localPosition;
+                dir.Normalize();
+
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                dockingArrow2D.transform.localRotation = Quaternion.Euler(dockingArrow2D.transform.localRotation.x, 0, angle);
+                arrowDock2D.transform.localRotation = Quaternion.Euler(dockingArrow2D.transform.localRotation.x, 0, angle);                
 
                 //Activate the Dock and arrow 
                 dockingArrow2D.SetActive(true);
@@ -331,13 +345,30 @@ public class CameraFeedback : MonoBehaviour {
             Vector3 arrowVector = targetSphere.transform.position - feedbackAvatar_joint.position;
             arrowVector = arrowVector.normalized;
 
-            // Calculate middled vector
-            Vector3 boneVectorA = feedbackAvatar_joint.GetChild(0).transform.position - feedbackAvatar_joint.position;
-            boneVectorA = boneVectorA.normalized;
+            if (Mathf.Abs(arrowVector.x) > Mathf.Abs(arrowVector.z))
+            {
+                cameraPerspective = CameraPerspectives.Front;
+            }
 
-            Transform parent = feedbackAvatar_joint.parent.transform;
-            Vector3 boneVectorB = parent.position - feedbackAvatar_joint.position;
-            boneVectorB = boneVectorB.normalized;
+            else
+            {
+                if (left)
+                {
+                    cameraPerspective = CameraPerspectives.Left;
+                }
+                else
+                {
+                    cameraPerspective = CameraPerspectives.Right;
+                }
+            }
+
+            // Calculate middled vector
+            //Vector3 boneVectorA = feedbackAvatar_joint.GetChild(0).transform.position - feedbackAvatar_joint.position;
+            //boneVectorA = boneVectorA.normalized;
+
+            //Transform parent = feedbackAvatar_joint.parent.transform;
+            //Vector3 boneVectorB = parent.position - feedbackAvatar_joint.position;
+            //boneVectorB = boneVectorB.normalized;
 
             // Calculate normal for camera
             // Vector3 camNormal = Vector3.Cross(arrowVector, ((boneVectorA + boneVectorB) - feedbackAvatar_joint.position).normalized);
@@ -345,7 +376,80 @@ public class CameraFeedback : MonoBehaviour {
 
             //feedbackCamera.position = arrow3D.transform.position - camNormal.normalized * camDistance;
             //feedbackCamera.transform.LookAt(feedbackAvatar_joint.transform.position);
-            feedbackCamera.transform.position = (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * -Vector3.forward;
+
+            if (cameraMotion == CameraMotionStates.Jumping)
+            {
+                if (cameraPerspective == CameraPerspectives.Front)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.identity;
+                    feedbackCamera.transform.position = (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * -Vector3.forward;                    
+                }
+
+                if (cameraPerspective == CameraPerspectives.Behind)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    feedbackCamera.transform.position = (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.forward;
+                }
+
+                if (cameraPerspective == CameraPerspectives.Left)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    feedbackCamera.transform.position = (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.left;
+                }
+
+                if (cameraPerspective == CameraPerspectives.Right)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Euler(0, -90, 0);
+                    feedbackCamera.transform.position = (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.right;
+                }
+
+                if (cameraPerspective == CameraPerspectives.Up)
+                {
+
+                }
+
+                if (cameraPerspective == CameraPerspectives.Normal)
+                {
+
+                }
+            }
+
+            if (cameraMotion == CameraMotionStates.Moving)
+            {
+                if (cameraPerspective == CameraPerspectives.Front)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Slerp(feedbackCamera.transform.rotation, Quaternion.identity, Time.deltaTime * 1);
+                    feedbackCamera.transform.position = Vector3.Slerp(feedbackCamera.transform.position, (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * -Vector3.forward, Time.deltaTime * 1);
+                }
+
+                if (cameraPerspective == CameraPerspectives.Behind)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Slerp(feedbackCamera.transform.rotation, Quaternion.Euler(0, 180, 0), Time.deltaTime * 1);
+                    feedbackCamera.transform.position = Vector3.Slerp(feedbackCamera.transform.position, (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.forward, Time.deltaTime * 1);
+                }
+
+                if (cameraPerspective == CameraPerspectives.Left)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Slerp(feedbackCamera.transform.rotation,Quaternion.Euler(0, 90, 0), Time.deltaTime * 1);
+                    feedbackCamera.transform.position = Vector3.Slerp(feedbackCamera.transform.position, (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.left, Time.deltaTime * 1);
+                }
+
+                if (cameraPerspective == CameraPerspectives.Right)
+                {
+                    feedbackCamera.transform.rotation = Quaternion.Slerp(feedbackCamera.transform.rotation, Quaternion.Euler(0, -90, 0), Time.deltaTime * 1);
+                    feedbackCamera.transform.position = Vector3.Slerp(feedbackCamera.transform.position, (feedbackAvatar_joint.position + targetSphere.transform.position) * 0.5f + camDistance * Vector3.right, Time.deltaTime * 1);
+                }
+
+                if (cameraPerspective == CameraPerspectives.Up)
+                {
+
+                }
+
+                if (cameraPerspective == CameraPerspectives.Normal)
+                {
+
+                }
+            }
         }
     }
 
